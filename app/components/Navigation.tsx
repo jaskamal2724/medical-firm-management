437632e4e6d029914c87165c6cdc74bc061789d3
jaskamal2724 <jaskamalsingh7872@gmail.com>
2025-01-26 11:53:43 +0530

    navbar fixes

--- app/components/Navigation.tsx
+++ app/components/Navigation.tsx
@@ -12,11 +12,11 @@ const Navigation = () => {
   return (
     <div className="navbar bg-base-100 shadow-lg">
       <div className="flex-1">
-        <Link href="/" className="btn btn-ghost normal-case text-xl">
+        <Link href="/" className="btn btn-ghost normal-case text-lg md:text-xl">
           Medical Firm
         </Link>
       </div>
-      
+
       <div className="flex-none">
         <ul className="menu menu-horizontal px-1">
           <li>
